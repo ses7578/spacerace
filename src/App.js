a7f315ae999168291850d253617c5ebc4deebd17
ses7578 <ses7578@rit.edu>
2020-11-29 16:00:42 -0500

    Pushing changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Time50 from "./TimeLine50";
