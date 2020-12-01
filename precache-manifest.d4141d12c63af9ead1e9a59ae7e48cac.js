4c7c2b113cd0eff2f4a40bb3d4c7f53f72ea7841
ses7578 <ses7578@rit.edu>
2020-12-01 10:32:14 -0500

    Updates

---
+++ precache-manifest.d4141d12c63af9ead1e9a59ae7e48cac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb207d4f41cb40e39f11d04c4be5251",
+    "url": "/spacerace/index.html"
+  },
+  {
+    "revision": "4079ece40c6ee60c5629",
+    "url": "/spacerace/static/css/main.68a2aa61.chunk.css"
+  },
+  {
+    "revision": "e5614a5f05beda60c472",
+    "url": "/spacerace/static/js/2.22dc3a3a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/spacerace/static/js/2.22dc3a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4079ece40c6ee60c5629",
+    "url": "/spacerace/static/js/main.01fb5ea4.chunk.js"
+  },
+  {
+    "revision": "6cb156e67726c1ff9dbc",
+    "url": "/spacerace/static/js/runtime-main.13cd2f2a.js"
+  },
+  {
+    "revision": "6d0783fd68d3c0d69beff612d5b53fd4",
+    "url": "/spacerace/static/media/luna1.6d0783fd.jpg"
+  },
+  {
+    "revision": "6099842fdaa23fbcaa3260f73c528545",
+    "url": "/spacerace/static/media/nasa.6099842f.png"
+  },
+  {
+    "revision": "c17858ff8d7bfc52824ede6adae79e75",
+    "url": "/spacerace/static/media/spaceRaceLogo.c17858ff.jpg"
+  },
+  {
+    "revision": "f7b4fba6d7e4e0f32e1e3f15cc70405b",
+    "url": "/spacerace/static/media/sputnik.f7b4fba6.jpg"
+  }
+]);
